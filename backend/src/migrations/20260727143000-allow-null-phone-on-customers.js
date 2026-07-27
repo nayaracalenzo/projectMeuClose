c9b33ecd23459d43da673b843cb88d77040aee3b
nayaracalenzo <queiroz.14@hotmail.com>
2026-07-27 19:56:29 -0300

    add adjust

---
+++ backend/src/migrations/20260727143000-allow-null-phone-on-customers.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('customers', 'phone', {
+      type: Sequelize.STRING(20),
+      allowNull: true,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      UPDATE "customers"
+      SET "phone" = ''
+      WHERE "phone" IS NULL;
+    `);
+
+    await queryInterface.changeColumn('customers', 'phone', {
+      type: Sequelize.STRING(20),
+      allowNull: false,
+    });
+  },
+};
